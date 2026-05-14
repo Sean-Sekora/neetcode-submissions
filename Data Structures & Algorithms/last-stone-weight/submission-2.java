dd4401c77a05ff350e4d69f59c4b1b93160f8993
Sekora <sean@sekora.io>
2026-05-14 11:14:02 -0400

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            heap.offer(stone);
+        }
+        while (heap.size() > 1) {
+            int s1 = heap.poll();
+            int s2 = heap.poll();
+            if (s1 > s2) {
+                heap.offer(s1 - s2);
+            }
+        }
+        return heap.isEmpty() ? 0 : heap.poll();
+    }
+}
